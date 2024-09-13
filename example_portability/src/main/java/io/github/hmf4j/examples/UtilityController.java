619ae6fd228928bb36aec9d0d0f8908971d42c25
aronsajan@hotmail.com <aronsajan@hotmail.com>
2024-09-12 20:20:29 -0400

    updates to docker compose and message generation controller

--- example_portability/src/main/java/io/github/hmf4j/examples/UtilityController.java
+++ example_portability/src/main/java/io/github/hmf4j/examples/UtilityController.java
@@ -2,10 +2,16 @@ package io.github.hmf4j.examples;
 
 import com.google.type.DateTime;
 import io.github.handy.messaging.types.simplemessage.SimpleMessage;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.Base64;
 import java.util.Date;
@@ -14,8 +20,8 @@ import java.util.Optional;
 @RestController
 public class UtilityController {
 
-    @RequestMapping(value = "/message", method = RequestMethod.GET)
-    public String getMessage(){
+    @RequestMapping(value = "/message", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public byte[] getMessage() throws IOException {
         SimpleMessage msg = new SimpleMessage();
         msg.setMessageId("msg1");
         msg.setContentSchema(String.class.getName());
@@ -23,6 +29,9 @@ public class UtilityController {
         msg.setSender("TestApp");
         msg.setPayload("Hello, this is a sample message from testApp".getBytes());
         msg.buildMessage();
-        return Base64.getEncoder().encodeToString(msg.serialize());
+        try(FileOutputStream fileOut = new FileOutputStream("message.bin")){
+            fileOut.write(msg.serialize());
+        }
+        return msg.serialize();
     }
 }
